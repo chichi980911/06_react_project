625247b34e5eafd19d9a3ef8869d8254d7e3a8ee
chichi980911 <shkim3657@nate.com>
2023-09-14 12:15:23 +0900

    commit

---
+++ 리액트연습1/frienddata/src/FriendItem.js
@@ -0,0 +1,17 @@
+const FriendItem = ({id, name, hobby, birthday, onDelete }) => {
+  return(
+          <tr>
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{hobby}</td>
+              <td>{birthday}</td>
+              <button onClick={()=>{
+                  if(window.confirm(`${id}번째 짝꿍을 삭제하실껀가요 ?`)){
+                      onDelete(id);
+                  }
+              }}>삭제하기</button>
+          </tr>
+  )
+}
+
+export default FriendItem;
---
+++ 리액트연습1/frienddata/src/data.js
@@ -0,0 +1,23 @@
+let data = [
+  {
+      id : '1',
+      name : "김상호",
+      hobby : "낚시하기",
+      birthday : "1998-01-01"
+  },
+  {
+      id : '2',
+      name : "윤관현",
+      hobby : "공부하기",
+      birthday : "1993-01-02"
+  },
+  {
+      id : '3',
+      name : "홍사원",
+      hobby : "잠자기",
+      birthday : "1993-01-03"
+  },
+
+];
+
+export default data;
